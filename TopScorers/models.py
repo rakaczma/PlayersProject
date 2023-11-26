6c6bb38a1ae4093d0a834442f672bbe1265e6112
rakaczma <rakaczma@student.wszib.edu.pl>
2023-11-27 00:27:54 +0100

    upd4

--- TopScorers/models.py
+++ TopScorers/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Player(models.Model):
     player_id = models.AutoField(primary_key=True)
     player_name = models.CharField(max_length=255)
-    club = models.OneToOneField('Club', on_delete=models.CASCADE, null=True, blank=True)
+    # club = models.OneToOneField('Club', on_delete=models.CASCADE, null=True, blank=True)
+    club = models.ForeignKey('Club', on_delete=models.CASCADE)
     stats = models.OneToOneField('Stats', on_delete=models.CASCADE, null=True, blank=True)
 
     def get_club_detail_url(self):
--- TopScorers/views.py
+++ TopScorers/views.py
@@ -5,34 +5,40 @@ from django.contrib.auth import login, logout
 from .forms import RegistrationForm, PlayerForm
 from django.contrib.auth.views import LoginView
 
+
 def player_list(request):
     players = Player.objects.select_related('club').all()
     return render(request, 'player_list.html', {'players': players})
+
 
 def club_list(request):
     clubs = Club.objects.all()
     return render(request, 'club_list.html', {'clubs': clubs})
 
+
 def stats_list(request):
     stats = Stats.objects.all()
     return render(request, 'stats_list.html', {'stats': stats})
+
 
 def player_detail(request, player_id):
     player = get_object_or_404(Player, pk=player_id)
     return render(request, 'player_detail.html', {'player': player, 'stats': player.stats})
 
+
 def club_detail(request, club_id):
     club = get_object_or_404(Club, pk=club_id)
-    return render(request, 'club_detail.html', {'club': club_name})
+    return render(request, 'club_detail.html', {'club': club_id})
+
 
 def stats_detail(request, stats_id):
     stats = get_object_or_404(Stats, pk=stats_id)
     return render(request, 'stats_detail.html', {'stats': stats})
 
 
-def add_player(request):
-    # Tworzenie piłkarza...
-    return render(request, 'add_player.html')
+# def add_player(request):
+#     # Tworzenie piłkarza...
+#     return render(request, 'add_player.html')
 
 
 def add_stats(request):
@@ -63,13 +69,31 @@ def login(request):
 @login_required
 def add_player(request):
     if request.method == 'POST':
-        form = PlayerForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('player_list')  # Przekieruj usera po dodaniu gracza
-    else:
-        form = PlayerForm()
-    return render(request, 'add_player.html', {'form': form})
+        player_name = request.POST.get('player_name')
+        club_name = request.POST.get('club_name')
+        club_country = request.POST.get('club_country')  # Dodane pole 'Kraj'
+        club_league = request.POST.get('club_league')    # Dodane pole 'Liga'
+
+        # Tworzenie klubu (jeśli nie istnieje)
+        club, created = Club.objects.get_or_create(
+            club_name=club_name,
+            defaults={
+                'country': club_country,
+                'league': club_league
+            }
+        )
+
+        # Tworzenie gracza i przypisanie klubu
+        player = Player.objects.create(
+            player_name=player_name,
+            club=club
+            # Dodaj tutaj inne pola gracza, jeśli istnieją
+        )
+
+        # Przekierowanie na stronę z listą graczy
+        return redirect('player_list')
+
+    return render(request, 'add_player.html')
 
 
 
